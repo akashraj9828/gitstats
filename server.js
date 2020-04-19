4f6cdb9d88b019579e807b1e3aca63a630ffd4b5
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-19 23:11:12 +0530

    #fix heroku

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-//EXPRESS server to server build files on heroku
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 4000;
-const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
-    return res.send('pong');
-});
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port, () => console.log("Server listeing on : ", port));
+
+// Using express: http://expressjs.com/
+var express = require('express');
+// Create the app
+var app = express();
+var server = app.listen(process.env.PORT || 4000, listen);
+
+
+
+app.use(express.static('./build/'));
+
+// // This call back just tells us that the server has started
+function listen() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('React app live at http://' + host + ':' + port);
+}
