fbc0542c09175318b1f15d53027e3aff8e097060
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-19 22:56:54 +0530

    changes for heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+//EXPRESS server to server build files on heroku
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 4000;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port, () => console.log("Server listeing on : ", port));
