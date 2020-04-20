6c6659f21854070d0ea671792958aa65dc5152c9
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-20 23:15:24 +0530

    removed logs

--- src/Components/Frontend-views/Pinned-view.js
+++ src/Components/Frontend-views/Pinned-view.js
@@ -5,12 +5,10 @@ const Pinned = (props) => {
     const [repoData, setRepoData] = useState([]);
     const username = props.username ? props.username : "akashraj9828"
     const getRepoUrl = window.API_BASE_URL + "pinned/" + username
-    console.log(getRepoUrl);
 
     const getPinnedRepo = async () => {
         let response = await axios.get(getRepoUrl);
         const pinned_data = response.data;
-        console.log(pinned_data);
         setRepoData(pinned_data.data.user.pinnedRepositories.nodes);
     };
 
@@ -19,40 +17,34 @@ const Pinned = (props) => {
         getPinnedRepo();
     }, []);
 
-    repoData.forEach(repo => {
+    let pinned_view = repoData.map((repo) =>
+        <div key={repo.name} className="col-md-4 my-3">
 
-    });
-
-    let pinned_view = repoData.map((repo) => {
-        return (
-            <div key={repo.name} className="col-md-4 my-3">
-
-                <div className="card h-100 rounded">
-                    <div className="card-block py-3 px-4">
-                        <h6 className="card-title text-center"> {repo.nameWithOwner}</h6>
-                        <div className="row text-center mt-3">
-                            <div className="col-md-4">
-                                {"-"}
-                                <br />
+            <div className="card h-100 rounded">
+                <div className="card-block py-3 px-4">
+                    <h6 className="card-title text-center"> {repo.nameWithOwner}</h6>
+                    <div className="row text-center mt-3">
+                        <div className="col-md-4">
+                            {"-"}
+                            <br />
                                 Commits
                             </div>
-                            <div className="col-md-4">
-                                {repo.forkCount}
-                                <br />
+                        <div className="col-md-4">
+                            {repo.forkCount}
+                            <br />
                                 Forks
                             </div>
-                            <div className="col-md-4">
-                                {repo.stargazers.totalCount}
-                                <br />
+                        <div className="col-md-4">
+                            {repo.stargazers.totalCount}
+                            <br />
                                 Stars
                             </div>
-                        </div>
-                        <h6 className="text-center mt-3">The main langiage is <span style={{ color: repo.languages.nodes[0].color }}>{repo.languages.nodes[0].name}</span></h6>
                     </div>
+                    <h6 className="text-center mt-3">The main langiage is <span style={{ color: repo.languages.nodes[0].color }}>{repo.languages.nodes[0].name}</span></h6>
                 </div>
             </div>
-        )
-    })
+        </div>
+    )
 
     return (
         <div className="row">
