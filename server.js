18cf4591e5e0dc3d074bac5f3322475dee1576a9
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-20 20:04:42 +0530

    heroku express fix

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+const express = require('express');
+const path = require('path');
 
-// Using express: http://expressjs.com/
-var express = require('express');
-// Create the app
-var app = express();
-var server = app.listen(process.env.PORT || 4000, listen);
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '/build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+});
 
 
 
-app.use(express.static('./build/'));
+var server = app.listen(process.env.PORT || 3000, listen);
 
-// // This call back just tells us that the server has started
+// This call back just tells us that the server has started
 function listen() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('React app live at http://' + host + ':' + port);
 }
+
+
