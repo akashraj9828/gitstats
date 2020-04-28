332f2d2c941386158ef6778bbf4bba7242ab2ef0
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-28 23:41:15 +0530

    default image size too large in activity tab fix

--- src/Components/Views/UserActvity.js
+++ src/Components/Views/UserActvity.js
@@ -22,7 +22,7 @@ function UserActivity(props) {
 
     let dom = data.map(event => <div key={event.id} className="row my-1">
         <div className="px-2">
-            <img className="img img-fluid rounded" src={event.image} alt="" />
+            <img className="img img-fluid rounded" style={{maxWidth:"30px"}} src={event.image} alt="" />
         </div>
         <div className="col">
             <h6 className="m-0 font-size-11">
