5dd15af1057f4bb409715a2dd50bdaa3f8c5a3b4
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-29 12:56:02 +0530

    Fixed crash in case pinned item is a gist

--- src/Components/Views/Pinned.js
+++ src/Components/Views/Pinned.js
@@ -5,16 +5,18 @@ const Pinned = (props) => {
     // deprecated api
     // const repoData = props.pinnedRepos.data.user.pinnedRepositories.nodes
 
-    const repoData = props.pinnedRepos.data.user.itemShowcase.items.nodes
+    let repoData = props.pinnedRepos.data.user.itemShowcase.items.nodes
+    // filter all empty repos // empty repos are returned when pinned item is a gist
+    repoData=repoData.filter((obj)=>Object.keys(obj).length !== 0 && obj.constructor)
     //HTML VIEW JSX DATA BIND
     let pinned_view = repoData.map((repo) =>
-        <div key={repo.name} className="col-md-4 my-3">
+    <div key={repo.name} className="col-md-4 my-3">
             <div className="card h-100 rounded">
                 <div className="card-block py-3 px-4">
 
                     <h6 className="card-title text-center">
                         <a className="pinned-repo-links" href={"http://github.com/" + repo.nameWithOwner} target="_blank" rel="noopener noreferrer">
-                            {repo.nameWithOwner}
+                            {repo.nameWithOwner || "---"}
                         </a> 
                     </h6>
                     <div className="row text-center mt-3">
@@ -24,18 +26,18 @@ const Pinned = (props) => {
                             <span className="mob-font-size-13"> Commits</span>
                         </div>
                         <div className="col-md-4 col-4">
-                            <span className="mob-font-size-13">{repo.forkCount}</span>
+                            <span className="mob-font-size-13">{repo.forkCount || 0}</span>
                             <br />
                             <span className="mob-font-size-13"> Forks</span>
                         </div>
                         <div className="col-md-4 col-4">
-                            <span className="mob-font-size-13">{repo.stargazers.totalCount}</span>
+                            <span className="mob-font-size-13">{(repo.stargazers && repo.stargazers.totalCount) || 0}</span>
                             <br />
                             <span className="mob-font-size-13">  Stars</span>
                         </div>
                     </div>
                     <h6 className="text-center mt-3">
-                        {repo.languages.nodes.length > 0 ? <Fragment>The main language is <span style={{ color: repo.languages.nodes[0].color || "grey" }}>{repo.languages.nodes[0].name}</span></Fragment> : <Fragment>No main language found </Fragment>}
+                        {repo.languages && repo.languages.nodes.length > 0 ? <Fragment>The main language is <span style={{ color: repo.languages.nodes[0].color || "grey" }}>{repo.languages.nodes[0].name || "---"}</span></Fragment> : <Fragment>No main language found </Fragment>}
                     </h6>
                 </div>
             </div>
@@ -48,7 +50,6 @@ const Pinned = (props) => {
             textAlign: "center",
             alignItems: "center"
         }}>
-
             <h6 className="mt-3 w-100 text-left">
                 <span style={{ color: "gray" }}>No pinned repos to analyze <span role="img" aria-label="sad-face">☹️☹️</span></span>
             </h6>
