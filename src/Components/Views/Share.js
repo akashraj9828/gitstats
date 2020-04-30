f5cc752c8f8c8a2dc6c455ec961f7fa916622b46
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-30 12:59:35 +0530

    download as image option added icon size increased to 40px

--- src/Components/Views/Share.js
+++ src/Components/Views/Share.js
@@ -2,48 +2,42 @@ import React from 'react'
 import { FacebookShareButton, LinkedinShareButton, TwitterShareButton, WhatsappShareButton, } from "react-share";
 import { FacebookIcon, LinkedinIcon, TwitterIcon, WhatsappIcon, } from "react-share";
 import { ReactComponent as DownloadIcon } from "./../../Images/download.svg"
-// import jsPDF from 'jspdf';
-// import html2canvas from 'html2canvas';
-import html2pdf from 'html2pdf.js';
 import config from '../../config/index'
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
+
 
 Object.keys(config).forEach(key => {
   window[key] = config[key];
 });
-function download(data) {
-  console.log("---: download -> data", data);
-
-  var element = document.getElementById('user-profile');
-
-  // var scale = (screen.width / page.getViewport(1.0).width);
-  //       var viewport = page.getViewport(scale);
-  //       canvas.height = viewport.height;
-  //       canvas.width = viewport.width;
-        
-  let html2canvasOpts = {
-    scale: 0.2,       //  img_width/win_width // is 1,
-    width: 150,
-    windowWidth: window.innerWidth,
-    windowHeight: window.innerHeight,
-  };
-
-  html2pdf(element, html2canvasOpts);
+function download({ data }) {
+  let filename = data.user.name || data.user.login
+  var activity_card = document.getElementById('user-activity')
+  // hide scrollbar before capture
+  activity_card.style.overflow = "hidden"
+  var node = document.getElementById('user-profile');
+  domtoimage.toBlob(node)
+    .then(function (blob) {
+      saveAs(blob, filename + '_Gitstats.png');
+      // enable scrollbar before capture
+      activity_card.style.overflow = "auto"
+    });
 }
 
 const Share = ({ data }) => {
   return (
     <div className="share-icons rounded">
       <FacebookShareButton url={window.location.href}>
-        <FacebookIcon size={28} round={true} />
+        <FacebookIcon size={40} round={true} />
       </FacebookShareButton>
       <TwitterShareButton url={window.location.href}>
-        <TwitterIcon size={28} round={true} />
+        <TwitterIcon size={40} round={true} />
       </TwitterShareButton>
       <WhatsappShareButton url={window.location.href}>
-        <WhatsappIcon size={28} round={true} />
+        <WhatsappIcon size={40} round={true} />
       </WhatsappShareButton>
       <LinkedinShareButton url={window.location.href}>
-        <LinkedinIcon size={28} round={true} />
+        <LinkedinIcon size={40} round={true} />
       </LinkedinShareButton>
       <button
         aria-label="linkedin"
@@ -55,6 +49,7 @@ const Share = ({ data }) => {
           font: "inherit",
           color: "inherit",
           cursor: "pointer",
+          width: 40
         }}>
         <DownloadIcon />
       </button>
