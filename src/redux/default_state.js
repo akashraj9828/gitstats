18f9c6e2426d88ae7ff717c5c900c6a515700c03
Akash raj(CMD) <akashraj9828@gmail.com>
2020-07-29 15:59:50 +0530

    Step 1 of escaping callback hell

--- src/redux/default_state.js
+++ src/redux/default_state.js
@@ -1,5 +1,12 @@
 export const default_state = {
 	app: {
 		theme: "light",
+		user_id: "", //userid
+		// from api1
+		basicInfo: null, //basic info // name // email // followers //following ,etc
+		pinnedInfo: null, //pinned repos
+
+		// from api2
+		repoInfo: null, // all repos upto 100
 	},
 };
